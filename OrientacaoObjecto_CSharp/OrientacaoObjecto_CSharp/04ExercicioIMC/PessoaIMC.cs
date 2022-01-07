832301df3946e5183baf3c1ceacfc8e5efe239e4
DavidSanculane-dev <sanculane.dev@gmail.com>
2022-01-07 11:22:25 +0200

    Calculco IMC

---
+++ OrientacaoObjecto_CSharp/OrientacaoObjecto_CSharp/04ExercicioIMC/PessoaIMC.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjecto_CSharp.ExercicioIMC
+{
+    public class PessoaIMC
+    {
+        //Atributos
+
+        public double peso;
+        public double altura1;
+        public double altura2;
+
+        //Metodos
+
+        public double IMC()
+        {
+            return peso / (altura1 * altura2);
+        }
+
+        //Situação
+        public string situacaoIMC(double IMC)
+        {
+            string retorno;
+
+            if (IMC < 18.5 )
+            {
+               retorno = "Abaixo do Peso";
+            }
+            if (IMC < 25)
+            {
+                retorno= "Peso normal";
+            }
+            if (IMC < 30)
+            {
+                retorno= "Acima do peso";
+            }
+            if (IMC < 35)
+            {
+                retorno= "Obesidade I";
+            }
+            if (IMC < 40)
+            {
+                retorno = "Obesidade II";
+            }
+            else 
+            {
+                retorno= "Obesidade III";
+            }
+
+            return retorno;
+        }
+       
+        
+        //Mensagem
+        
+        public void mensagee()
+        {
+            double obterIMC = IMC();
+
+            string obterSituacao = situacaoIMC(obterIMC);
+            Console.WriteLine("O IMC é de: " +obterIMC);
+            Console.WriteLine("A sua situação é: " + obterSituacao);
+            Console.ReadKey();
+        }
+    }
+}
--- OrientacaoObjecto_CSharp/OrientacaoObjecto_CSharp/Program.cs
+++ OrientacaoObjecto_CSharp/OrientacaoObjecto_CSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using OrientacaoObjecto_CSharp._01Conceitos;
 using OrientacaoObjecto_CSharp._02Métodos;
 using OrientacaoObjecto_CSharp._03ExercicioEscolar;
+using OrientacaoObjecto_CSharp.ExercicioIMC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,9 +24,15 @@ namespace OrientacaoObjecto_CSharp
 
             Aluno a = new Aluno();
             a.nome = "Sanculane";
-            a.nota1 = 15;
+            a.nota1 = 7;
             a.nota2 = 4;
             a.mensage();
+
+            PessoaIMC imc = new PessoaIMC();
+            imc.peso = 68;
+            imc.altura1 = 1.69;
+            imc.altura2 = 1.69;
+            imc.mensagee();
         }
     }
 }
